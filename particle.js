2d9ac6bfa20b0b4ae86daf832706185627b35a96
Krish-Mittal <69497201+Krish-Mittal@users.noreply.github.com>
2020-11-18 15:15:47 +0530

    Update particle.js

--- particle.js
+++ particle.js
@@ -8,12 +8,12 @@ class part
         }
         this.body=Body.circle(x,y,r,op)
         this.r=r;
-        //this.color=random(random(0,255),random(0,255),random(0,255))
+        this.color=random(random(0,255),random(0,255),random(0,255))
         World.add(world,this.body);
     }
     display()
     {
-        fill("white")
+        fill(this.color)
         ellipseMode(CENTER);
        
         ellipse(this.body.position.x,this.body.position.y,this.r)
